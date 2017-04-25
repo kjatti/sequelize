6f08f26c083b0542a3be7aa81dc8b3166f462e6a
Karthik Jatti <kjatti@ezesoft.com>
2017-04-25 17:56:26 -0400

    working hack - no tests or lint passing

---
+++ dev-test.js
@@ -0,0 +1,65 @@
+var Sequelize = require("./index");
+
+var sequelize = new Sequelize("sequelize-test", "sa", "ezetc", {
+    host: 'localhost',
+    dialect: "mssql",
+    dialectOptions: {
+        encrypt: true
+    }
+})
+
+var User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING,
+        field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        field: 'last_name'
+    },
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+},
+    {
+        freezeTableName: true // Model tableName will be the same as the model name
+    });
+
+
+var Post = sequelize.define('post', {
+    content: {
+        type: Sequelize.STRING,
+    },
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    }
+});
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+sequelize.sync({ force: true }).then(function () {
+
+    User.bulkCreate(
+        [
+            { firstName: "Karthik", lastName: "Jatti" },
+            { firstName: "shuai", lastName: "zhang" },
+            { firstName: "bob", lastName: "marley" },
+            { firstName: "nick", lastName: "tzaprev" }
+        ]
+
+    )
+        .then(() => {
+            sequelize.query("SELECT * FROM dbo.[user] where first_name = $firstName OR last_name = $lastName", {
+                bind: {
+                    firstName: "Karthik",
+                    lastName: "Tzaprev"
+                }
+            }).then(x => console.log(x));
+        });
+
+});
